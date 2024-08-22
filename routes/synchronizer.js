29beb34b23deb5d0d07af01a2d98feb010801d40
Satrio Wibowo <satriowibowo@Satrios-MacBook-Pro.local>
2024-08-22 19:53:49 +0700

    update

--- routes/synchronizer.js
+++ routes/synchronizer.js
@@ -11,45 +11,9 @@ router.post('/task_date', async function (req, res, _next) {
     }
 });
 
-router.post('/ideation', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.relationSync(req.body.payload));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
-router.post('/counter_feedback_staging', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.counterFeedbackStaging(req.body.payload, req.query.type));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
-router.post('/counter_feedback_prod', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.counterFeedbackProd(req.body.payload, req.query.type));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
 router.post('/subtask_sync', async function (req, res, _next) {
     try {
         res.json(await synchronizer.subtaskSync(req.body.payload, req.query.type));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
-router.post('/auto_complete_feedback_enterprise', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.autoCompleteStatus(req.body.payload, req.query.type));
     } catch (err) {
         console.error(err.message);
         res.status(err.statusCode || 500).json({ 'message': err.message });
--- services/synchronizer.js
+++ services/synchronizer.js
@@ -6,13 +6,8 @@ const asyncFilter = require('../utils/filterAsync');
 const mongo = require('./mongo');
 
 
-const webhook_cf_id = "c8b26f24-d304-4bff-9a27-2de70e8bae02"
-const mandays_cf_id = "49bcb816-b264-430f-bf51-6cc390787234"
-const feedback_stg_cf_id = "b04b9446-4f56-47cd-ada0-2e379268fe40"
-const feedback_prod_cf_id = "ce85b095-d027-4215-a936-778387e0c2f0"
+const webhook_cf_id = "a003363d-a9ab-4acf-a33b-93b4ae5fd430"
 const epic_release_cf_id = "dccb4f61-d762-4403-b560-b452216e34d2"
-const reviewer_cf_id = "68dfad2d-8ec7-4d52-a406-9e849f0cbe2b"
-const pm_cf_id = "bfd922ae-695a-41c1-9af7-e55ea38b8252"
 const theme_cf_id = "71aa8337-49a5-4ba8-986e-409b46049e4e"
 const quarter_cf_id = "ecb1c819-265d-42bb-918b-bc73d7df93c6"
 
@@ -26,16 +21,6 @@ async function dateSync(payload, type) {
         let start_date = moment.unix(task.start_date) || false;
         let pointer = (task.parent) ? task.parent : false;
         let duration = parseInt(moment.duration(due_date.diff(start_date)).asDays());
-
-        // tambahkan fungsi hitung mandays ketika ada trigger ubah mandays
-        // await axios({
-        //     method: "POST",
-        //     url: `https://api.clickup.com/api/v2/task/${task.id}/field/${mandays_cf_id}`,
-        //     data: {
-        //         "value": ( duration / 1000)
-                
-        //     }
-        // });
 
         while (pointer) {
             let parent = await axios({
@@ -75,22 +60,6 @@ async function dateSync(payload, type) {
                         url: `https://api.clickup.com/api/v2/task/${pointer}`,
                         data: date_payload
                     });
-                    // if (date_updated) {
-                        // await axios({
-                        //     method: "POST",
-                        //     url: `https://api.clickup.com/api/v2/task/${pointer}/field/${mandays_cf_id}`,
-                        //     data: {
-                        //         "value": moment.duration(moment(date_updated.data.due_date)).diff(moment(date_updated.data.start_date)).asDays()
-                        //     }
-                        // });    
-                        // await axios({
-                        //     method: "POST",
-                        //     url: `https://api.clickup.com/api/v2/task/${pointer}/comment`,
-                        //     data: {
-                        //         comment_text: '(BOT) Date changed because of date change on https://app.clickup.com/t/' + task.id
-                        //     }
-                        // });
-                    // }
                     await axios({
                         method: "DELETE",
                         url: `https://api.clickup.com/api/v2/task/${pointer}/field/${webhook_cf_id}`
@@ -101,143 +70,6 @@ async function dateSync(payload, type) {
             pointer = parent.parent
         }
 
-        return 'OK'
-    } catch (error) {
-        console.log("====== Start Err ClickUp =====")
-        console.log(error)
-        console.log("====== End Err ClickUp =====")
-    }
-}
-
-async function relationSync(payload) {
-    // if (config.debug) mongo.insertLogs(payload);
-    let ideation_field_id = "4f9363bd-08fd-4f20-8cd9-814bb96453fc"
-    try {
-        let task = payload;
-        let due_date = moment.unix(task.due_date) || false;
-        let start_date = moment.unix(task.start_date) || false;
-
-        let custom_fields = task.custom_fields;
-        // filter by custom field id
-        let pointers = await asyncFilter(custom_fields, async (i) => {
-            return i.id == ideation_field_id;
-        });
-
-        if (typeof pointers.value == 'undefined') return 'OK';
-
-        for (let pointer of pointers.value) {
-            let parent = await axios({
-                method: "GET",
-                url: `https://api.clickup.com/api/v2/task/${pointer.id}`
-            });
-            parent = parent.data
-
-            let parent_start_date = moment.unix(parent.start_date) || false;
-            let parent_due_date = moment.unix(parent.due_date) || false;
-
-            let duration_start_date = (parent_start_date) ? moment.duration(parent_start_date.diff(start_date)).asDays() : false;
-            let duration_due_date = (parent_due_date) ? moment.duration(parent_due_date.diff(due_date)).asDays() : false;
-
-            if (parent_due_date && parent_due_date > due_date && parent_due_date.unix() > 0) {
-                due_date = parent_due_date
-            }
-            if (parent_start_date && parent_start_date < start_date && parent_start_date.unix() > 0) {
-                start_date = parent_start_date
-            }
-
-            let cf_updated = await axios({
-                method: "POST",
-                url: `https://api.clickup.com/api/v2/task/${pointer}/field/${webhook_cf_id}`,
-                data: {
-                    "value": 1
-                }
-            });
-
-            if (cf_updated) {
-                let date_payload = {};
-                if (due_date && due_date.unix() > 0 && duration_due_date && duration_due_date !== 0) date_payload.due_date = due_date.unix();
-                if (start_date && start_date.unix() > 0 && duration_start_date && duration_start_date !== 0) date_payload.start_date = start_date.unix();
-                if (date_payload.due_date || date_payload.start_date) {
-                    let date_updated = await axios({
-                        method: "PUT",
-                        url: `https://api.clickup.com/api/v2/task/${pointer}`,
-                        data: date_payload
-                    });
-                    // if (date_updated) {
-                    //     await axios({
-                    //         method: "POST",
-                    //         url: `https://api.clickup.com/api/v2/task/${pointer}/comment`,
-                    //         data: {
-                    //             comment_text: '(BOT) Date changed because of date change on https://app.clickup.com/t/' + task.id
-                    //         }
-                    //     });
-                    // }
-                    await axios({
-                        method: "DELETE",
-                        url: `https://api.clickup.com/api/v2/task/${pointer}/field/${webhook_cf_id}`
-                    });
-                }
-            }
-        }
-
-        return 'OK'
-    } catch (error) {
-        console.log("====== Start Err ClickUp =====")
-        console.log(error)
-        console.log("====== End Err ClickUp =====")
-    }
-}
-
-// untuk menambahkan counter pada custom field COUNTER FEEDBACK STAGING saat perubahan status dari QA Test Staging ke Rejected by QA Staging
-async function counterFeedbackStaging(payload) {
-    try {
-        let task = payload
-        // let counter = task.custom_fields[1].value ? task.custom_fields[1].value : 0;
-
-        let counter = await asyncFilter(task.custom_fields, async (i) => {
-            return i.id == feedback_stg_cf_id;
-        });
-        console.log(counter);
-        counter = counter[0].value ? counter[0].value : 0;
-        console.log(counter);
-        
-        await axios({
-            method: "POST",
-            url: `https://api.clickup.com/api/v2/task/${task.id}/field/${feedback_stg_cf_id}`,
-            data: {
-                "value": parseInt(counter)+1
-            }
-        });
-        
-        return 'OK'
-    } catch (error) {
-        console.log("====== Start Err ClickUp =====")
-        console.log(error)
-        console.log("====== End Err ClickUp =====")
-    }
-}
-
-// untuk menambahkan counter pada custom field COUNTER FEEDBACK PROD saat perubahan status dari QA Test Prod ke Rejected by QA Prod
-async function counterFeedbackProd(payload) {
-    try {
-        let task = payload
-        // let counter = task.custom_fields[2].value ? task.custom_fields[2].value : 0;
-        
-        let counter = await asyncFilter(task.custom_fields, async (i) => {
-            return i.id == feedback_prod_cf_id;
-        });
-        console.log(counter);
-        counter = counter[0].value ? counter[0].value : 0;
-        console.log(counter);
-        
-        await axios({
-            method: "POST",
-            url: `https://api.clickup.com/api/v2/task/${task.id}/field/${feedback_prod_cf_id}`,
-            data: {
-                "value": parseInt(counter)+1
-            }
-        });
-        
         return 'OK'
     } catch (error) {
         console.log("====== Start Err ClickUp =====")
@@ -342,37 +174,6 @@ async function subtaskSync(payload) {
             });
         }
 
-        
-        return 'OK'
-    } catch (error) {
-        console.log("====== Start Err ClickUp =====")
-        console.log(error)
-        console.log("====== End Err ClickUp =====")
-    }
-}
-
-// untuk otomatisasi status card feedback enterprise menjadi DONE ketika card developmnent telah COMPLETE
-async function autoCompleteStatus(payload) {
-    try {
-        let task = payload
-        
-        let task_status = task.status.status || false;
-        let linked_card = task.linked_tasks[0].task_id || false;
-
-        console.log(task_status)
-        console.log(linked_card)
-
-        if(task_status == "complete"){
-            //ubah status card dependency menjadi DONE
-            console.log('ubah status card dependency (https://app.clickup.com/t/'+linked_card+') menjadi DONE');
-            await axios({
-                method: "PUT",
-                url: `https://api.clickup.com/api/v2/task/${linked_card}`,
-                data: {
-                    "status": "done"
-                }
-            });
-        }
         
         return 'OK'
     } catch (error) {
