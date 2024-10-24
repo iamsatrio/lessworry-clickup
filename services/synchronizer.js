8135486297868989e149bb6ea2dfd68ee789e932
Satrio Wibowo <satriowibowo@Satrios-MacBook-Pro.local>
2024-10-25 01:36:37 +0700

    tambahan1

--- services/synchronizer.js
+++ services/synchronizer.js
@@ -232,7 +232,11 @@ async function inboundStock(payload) {
             return i.id == ho_inbound_stock_cf_id;
         });
         console.log("===========================");
-        console.log(latest_inbound_stock_ho);
+        console.log(latest_inbound_stock_ho.value);
+        console.log("===========================");
+        console.log(`https://api.clickup.com/api/v2/task/${ho_item[0].id}/field/${ho_inbound_stock_cf_id}`);
+        console.log("===========================");
+        console.log(parseInt(latest_inbound_stock_ho.value)+parseInt(quantity))
         console.log("===========================");
         
             await axios({
